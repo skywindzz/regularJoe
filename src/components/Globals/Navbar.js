03fd298713273b76c898d7c3cb8c3171386ba649
skywindzz <skywindzz@yahoo.com>
2019-06-06 19:40:23 -0700

    6/6

--- src/components/Globals/Navbar.js
+++ src/components/Globals/Navbar.js
@@ -1,34 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
 import logo from '../../images/logo.svg'
-import { FaCartArrowDown } from 'react-icon/fa'
+import { FaCartArrowDown } from 'react-icons/fa'
 
-export default function Navbar() {
-    const [navbarOpen, navbarToggle] = useState(false);
-    const [collapse, show] = useState('collapse navbar-collapse');
-    const [links, navbarHandler] = useState(
-        [
-            {
-                id: 1,
-                path: '/',
-                text: 'home'
-            },
-            {
-                id: 2,
-                path: '/about',
-                text: about
-            }
-        ]
-    )
-        
-    navbarHandler = () => { 
-        console.log('testing')
-    }
-    render() {
-      return (
-            <div>
-                navbar content
-        </div>
-    )
+
+export default class Navbar extends Component { 
+    render() { 
+        return (
+            <div>hello</div>
+        )
     }
 }
