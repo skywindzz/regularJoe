a4d7c2073f621ba78f0452a5d2686997a9412795
skywindzz <skywindzz@yahoo.com>
2019-07-10 19:58:36 -0700

    7/10

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,34 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Background from '../components/Globals/Background'
 
 //check react icon doc for different icon aggregators /fa is for fontawsome, there are others
-import { FaGulp } from 'react-icons/fa'
 
-const IndexPage = () => (
+
+
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Regular" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <FaGulp />
+    
+    <Background
+      img={data.img.childImageSharp.fluid}
+      title="regular joe's"
+      styleClass="default-background" />
   </Layout>
-)
+); 
 
+export const query = graphql`
+{
+  img:file(relativePath:{eq:"default-background.jpeg"}){
+    childImageSharp{
+      fluid{
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+}
+`
 export default IndexPage
